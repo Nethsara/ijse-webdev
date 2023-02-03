dfb3e73c5098bcd2f189984cae5cd325ca2d92f5
NethsaraSiyum <nethsara@apium.io>
2023-02-03 11:54:42 +0530

    Hecker Initi

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const momsName = document.getElementById("momsName").value;
+});
